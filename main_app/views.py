14cb31502282381ce934c15df381e2bb29f6ed89
chinobin <sfaigon@email.com>
2023-11-21 11:41:46 -0500

    create view for employee url and views.py

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,7 @@ def home(request):
     return render(request, 'home.html')
 def about(request):
     return render(request, 'about.html')
+def employees_index(request):
+    return render(request, 'employees/index.html', {
+        'employees': employees
+    })
