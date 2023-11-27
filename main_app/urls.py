fe1eee7975e110aad1b1ad06f36a25b9c0493445
Abi Samson <abi.samson97@yahoo.com>
2023-11-27 09:40:54 -0500

    Create skills index route

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 	path('', views.home, name = 'home'),
     path('about', views.about, name='about'),
-    
+    path('skills/', views.SkillList.as_view(), name='skills_index'),
 ]
