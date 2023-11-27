63587e42520e94083f86fd252325f9e8e68cb3ec
Abi Samson <abi.samson97@yahoo.com>
2023-11-27 11:12:24 -0500

    update skills index view

--- main_app/views.py
+++ main_app/views.py
@@ -6,5 +6,6 @@ def home(request):
     return render(request, 'home.html')
 def about(request):
     return render(request, 'about.html')
-class SkillList (ListView):
+class SkillList(ListView):
     model = Skill
+    template_name = 'main_app/skill-list.html'
