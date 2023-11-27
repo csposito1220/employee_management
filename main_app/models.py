c8a24dab7289f5314d2e584f6ac9f2c9ec326815
chinobin <sfaigon@email.com>
2023-11-27 09:10:01 -0500

    a

--- main_app/models.py
+++ main_app/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
-    # Going to try ImageField, but can just make them add an image in 'detail' just like in catcollector
-    avatar = models.ImageField()
-    admin = models.BooleanField(
-        max_length=1,
-        default=False
-    )
     
 class User(models.Model):
     name = models.CharField(max_length=50)
