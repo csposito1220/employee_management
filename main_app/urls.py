ef97967836b65bee9577654393d2eb154725b88b
Abi Samson <abi.samson97@yahoo.com>
2023-11-27 13:48:04 -0500

    Create skills form route

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('employees/', views.employees_index, name='index'),
     path("employees/<int:employee_id>/", views.employees_details, name="detail"),
     path('skills/', views.SkillList.as_view(), name='skills_index'),
+    path('skills/create/', views.SkillCreate.as_view(), name='skill_create'),
 ]
