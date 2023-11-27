cf6e255eed0b6992ebe376c7398f26b8672561ca
Abi Samson <abi.samson97@yahoo.com>
2023-11-27 09:56:44 -0500

    Add skill model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    date= models.DateField()
+
+    def __str__(self):
+        return self.name
