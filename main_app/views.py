b270892960b0d9b5d21bdb2715e32a5d5f6ec4a9
Abi Samson <abi.samson97@yahoo.com>
2023-11-27 10:00:13 -0500

    Add skill index view

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Skill
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 def about(request):
     return render(request, 'about.html')
+class SkillList (ListView):
+    model = Skill
