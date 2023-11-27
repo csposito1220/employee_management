f0d9db2b2238676f886e7fc2d8f72ef0ef8725c1
csposito1220 <csposito1220@gmail.com>
2023-11-27 10:30:57 -0500

    Create accounts/ url

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,17 @@ def employees_index(request):
     return render(request, 'employees/index.html', {
         'employees': employees
     })
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
