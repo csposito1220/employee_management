b501d81e1909f3d9ca3435fb914c692c5302b218
Abi Samson <abi.samson97@yahoo.com>
2023-11-27 14:15:35 -0500

    update skill model's date field

--- main_app/models.py
+++ main_app/models.py
@@ -21,7 +21,7 @@ class Employee(models.Model):
 
 class Skill(models.Model):
     name = models.CharField(max_length=100)
-    date= models.DateField()
+    date= models.DateField('Date Earned')
 
     def __str__(self):
         return self.name
