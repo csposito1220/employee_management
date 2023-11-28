02b0512e9290906c54a55ea5d2daf48f1e23726d
csposito1220 <csposito1220@gmail.com>
2023-11-28 11:45:29 -0500

    create path for employee create

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('employees/', views.employees_index, name='index'),
     path("employees/<int:employee_id>/", views.employees_details, name="detail"),
+    path('employees/create/', views.EmployeeCreate.as_view(), name='employee_create'),
     path('skills/', views.SkillList.as_view(), name='skills_index'),
     path('skills/create/', views.SkillCreate.as_view(), name='skill_create'),
     
