bf5aa5d9c4a0a8a66f2cfefcaea40d2e6aef431b
Abi Samson <abi.samson97@yahoo.com>
2023-11-29 09:24:17 -0500

    Create route for employee update

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('employees/', views.employees_index, name='index'),
     path("employees/<int:employee_id>/", views.employees_details, name="detail"),
-    path('employees/create/', views.EmployeeCreate.as_view(), name='employee_create'),
     path('skills/', views.SkillList.as_view(), name='skills_index'),
     path('skills/create/', views.SkillCreate.as_view(), name='skill_create'),
     path('accounts/signup/', views.signup, name='signup'),
@@ -14,4 +13,6 @@ urlpatterns = [
     path('skills/<int:pk>/update/', views.SkillUpdate.as_view(), name='skills_update'),
     path('employees/<int:employee_id>/assoc_skill/', views.assoc_skill, name='assoc_skill'),
     path('employees/<int:employee_id>/unassoc_skill/<int:skill_id>/', views.unassoc_skill, name='unassoc_skill'),
+    path('employees/create/', views.EmployeeCreate.as_view(), name='employee_create'),
+    path('employees/<int:pk>/update/', views.EmployeeUpdate.as_view(), name='employees_update'),
 ]
