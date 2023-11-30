c5337e94731e740cfc45cc3e9beb870206f95a19
csposito1220 <csposito1220@gmail.com>
2023-11-29 23:09:23 -0500

    Update admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Employee, Skill, Position, Photo
 
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Skill)
+admin.site.register(Position)
+admin.site.register(Photo)
