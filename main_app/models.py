305b6a1efa6b2e0335be494a6fd9218c77732b41
Abi Samson <abi.samson97@yahoo.com>
2023-12-01 09:00:45 -0500

    Edit models

--- main_app/models.py
+++ main_app/models.py
@@ -10,18 +10,6 @@ LEVELS = (
     ('L5', 'LEVEL 5'),
 )
 
-# Create your models here.   
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=50)
-#     email = models.CharField(max_length=100)
-#     # Going to try ImageField, but can just make them add an image in 'detail' just like in catcollector
-#     # avatar = models.ImageField()
-#     admin = models.BooleanField(
-#         max_length=1,
-#         default=False
-#     )
-
 class Skill(models.Model):
     name = models.CharField(max_length=100)
     pay_increase = models.DecimalField(max_digits=5, decimal_places=2)
@@ -59,7 +47,7 @@ class Employee(models.Model):
     friday_start = models.TimeField(blank=True, null=True,  default='08:00')
     friday_end = models.TimeField(blank=True, null=True,  default='17:00')
     
-    # position = 
+   
     def __str__(self):
         return self.name
     def get_absolute_url(self):
